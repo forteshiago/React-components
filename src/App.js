89e72fa8434177bb150232fd830171e6a97b4255
unknown <you@example.com>
2020-09-29 22:12:26 -0300

    Creating base

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
-
+import Container from "./components/Container";
 import "./App.css";
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Container />
+      <ItemList />
+    </>
+  );
 }
 
 export default App;
